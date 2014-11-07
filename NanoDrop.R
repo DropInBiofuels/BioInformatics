8bc2b239905ca80f4af91a96616d04b2267e9a6b
bertbrutzel <bertbrutzel@gmail.com>
2014-11-07 14:30:18 +0100

    First version of a script that transforms nanodrop ndv files to graphs

---
+++ NanoDrop.R
@@ -0,0 +1,17 @@
+## Script to plot Nanodrop Data
+## J. Kabisch 2014
+## kabisch@uni-greifswald.de
+library(reshape)
+library(ggplot)
+header <- read.table("TALENJK0713033.ndv",nrows = 4, sep="\t",header=F, stringsAsFactors = FALSE)
+values <- read.table("TALENJK0713033.ndv", skip=4, sep="\t",header=T, dec = ",", comment.char="?")
+#transTest <- read.table("TALENJK0713033_test.csv", skip=4, sep="\t", header=T, dec=",")
+curve.values <- subset(values, select = c(Sample.ID, X220:X303)) # extract values for plots
+amount <- subset(values, select = c(Sample.ID, ng.ul)) # extract quantity
+protein <- subset(values, select = c(X260.280)) # extract protein contamination
+md <- melt(curve.values, id=(c("Sample.ID"))) # transform to ggplot format
+md$variable<-substring(md$variable,2) # remove X in front of variables
+p <- ggplot(md) + geom_line(aes(x=variable, y=value, group=Sample.ID)) + facet_wrap(~Sample.ID) + ylim(-1, 6)
+p <-p + annotate("text", x=300, y=6, label = c(~amount))
+p
+           
